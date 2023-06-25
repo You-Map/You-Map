28a977b056ca64239d2d4340e86a938def655434
yuyi5187 <kimyy200063@gmail.com>
2023-06-25 18:09:24 +0900

    API Test Completed

--- FE/src/App.js
+++ FE/src/App.js
@@ -2,7 +2,7 @@ import Router from "./Router";
 import GlobalStyle from "./style/GlobalStyle";
 import "./style/font.css";
 
-export const APIURL = process.env.REACT_APP_APIURL;
+export const APIURL = "http://127.0.0.1:8000"
 
 function App() {
   return (
--- FE/src/pages/Place.jsx
+++ FE/src/pages/Place.jsx
@@ -1,18 +1,21 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 import { APIURL } from "../App";
 
 const Place = () => {
-  const params = React.useParams();
+  const params = useParams();
   const getAPI = async () => {
     try {
-      const url = `${APIURL}/post/list-certified-loc/${params.placeID}`;
+      const url = `${APIURL}/post/list-certified-loc/?location=${params.placeID}`;
+      //console.log(url);
       const res = await axios.get(url);
       console.log(res.data);
     } catch (err) {
       console.log("getPost error: ", err);
     }
   };
+  getAPI();
   return <div></div>;
 };
 
