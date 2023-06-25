0a5852eb95c1152de4b6dfe287ccf9718d420d82
Goonco <s07019@naver.com>
2023-06-25 17:33:45 +0900

    login box temp

--- FE/src/components/LoginBox.jsx
+++ FE/src/components/LoginBox.jsx
@@ -1,13 +1,47 @@
 import styled from "styled-components";
+import { palette } from "../style/palette";
+
+import { useNavigate } from "react-router-dom";
 
 const LoginBox = () => {
-  return <LoginBoxContainer>Login</LoginBoxContainer>;
+  const nav = useNavigate();
+  const goMain = () => {
+    nav("/main");
+  };
+
+  return (
+    <LoginBoxContainer>
+      YouMap Login
+      <Input />
+      <Input />
+      <LoginButton onClick={goMain}>로그인</LoginButton>
+    </LoginBoxContainer>
+  );
 };
 
 const LoginBoxContainer = styled.div`
-  width: 200px;
-  height: 200px;
-  background-color: red;
+  width: 350px;
+  padding: 30px 0;
+  text-align: center;
+  background-color: white;
+  border-radius: 10px;
+`;
+
+const Input = styled.input`
+  display: block;
+  width: 65%;
+  margin: 20px auto;
+  padding: 5px;
+  border: 2px solid ${palette.brand_lv4};
+  border-radius: 10px;
+`;
+
+const LoginButton = styled.button`
+  background-color: ${palette.brand_lv4};
+  color: white;
+  width: 65%;
+  padding: 5px;
+  border-radius: 10px;
 `;
 
 export default LoginBox;
