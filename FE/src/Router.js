3b452d91c5dd4fc4fbe3508d2abd125efa1aac3f
Goonco <s07019@naver.com>
2023-06-25 12:38:55 +0900

    pagination branch create

--- FE/src/Router.js
+++ FE/src/Router.js
@@ -1,8 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>{/* <Route path="/" element={<Home />} /> */}</Routes>
     </BrowserRouter>
   );
--- FE/src/style/GlobalStyle.js
+++ FE/src/style/GlobalStyle.js
@@ -3,6 +3,11 @@ import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
     ${reset};
+    body {
+        max-width : 1920px;
+        min-width : 1080px;
+        margin : 0 auto;
+     }
 `;
 
 export default GlobalStyle;
