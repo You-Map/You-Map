6e76545571d8eaa8ec9b64f8edf4eb519e8afc6a
Goonco <s07019@naver.com>
2023-06-25 15:32:21 +0900

    IntroContent

--- FE/src/components/IntroContent.jsx
+++ FE/src/components/IntroContent.jsx
@@ -1,5 +1,36 @@
+import styled from "styled-components";
+
 const IntroContent = () => {
-  return <>IntroContent!</>;
+  return (
+    <IntroContentContainer>
+      <IntroContentTitle>
+        <h1>YouMap</h1>
+        <h2>너를 위한 지도</h2>
+      </IntroContentTitle>
+      <IntroContentDescription>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry.
+        <br />
+        Lorem Ipsum has been the industry's standard dummy text ever since the
+        1500s,
+        <br />
+        when an unknown printer took a galley of type and scrambled it to make a
+        type specimen book. <br />
+        It has survived not only five centuries, but also the leap into
+        electronic typesetting, remaining essentially
+      </IntroContentDescription>
+    </IntroContentContainer>
+  );
 };
+
+const IntroContentContainer = styled.div`
+  width: 100%;
+`;
+
+const IntroContentTitle = styled.div`
+  margin: 5rem auto;
+`;
+
+const IntroContentDescription = styled.p``;
 
 export default IntroContent;
